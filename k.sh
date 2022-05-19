9d8b4dcde78388c3205f46388019664618cc7db2
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-05-19 18:48:26 +0530

    Update k.sh

--- k.sh
+++ k.sh
@@ -6,7 +6,7 @@ sed -i s/demo1/${BOT_API_KEY}/g $HOME/.telegram.sh
 sed -i s/demo2/${CHAT_ID}/g $HOME/.telegram.sh
 
 git clone https://github.com/osm0sis/AnyKernel3 $HOME/AnyKernel3
-git clone https://github.com/arter97/arm64-gcc -b master --depth 1 $HOME/aarch64
+git clone https://github.com/sabmohmaya/arm64-gcc -b 9 --depth 1 $HOME/aarch64
 
 KDIR=$PWD
 TG=$HOME/telegram.sh/telegram
